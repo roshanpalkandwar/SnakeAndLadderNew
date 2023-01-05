7774c8201c6625c4a7c136cff9b6944b1fb2ada2
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-05 20:42:38 +0530

    [Roshan]Added:Player Start From Zero Possition

---
+++ SnackLadder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnackLadder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake Ladder Sumulation Program");
+            SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
+            snakeAndLadder.StartPlaying();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SnackLadder/SnakeAndLadder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnackLadder
+{
+    public class SnakeAndLadder
+    {
+        public int StartPoint = 0;
+        public void StartPlaying()
+        {
+            Console.WriteLine("Game Started! Good Luck ");
+            Console.WriteLine("You are at " + StartPoint);
+        }
+    }
+}

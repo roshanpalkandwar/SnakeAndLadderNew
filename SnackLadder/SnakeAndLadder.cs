ce3827ed6a852ef801031b8f07d9dfa3720155e6
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-05 21:01:08 +0530

    [Roshan]Added:Player Check OPtion For No Play,Ladder Or Snake

--- SnackLadder/SnakeAndLadder.cs
+++ SnackLadder/SnakeAndLadder.cs
@@ -10,6 +10,8 @@ namespace SnackLadder
     {
         public int StartPoint = 0;
         int RanDomDice;
+        int ComputerChoice;
+        int Option = 0;
         public void StartPlaying()
         {
             Console.WriteLine("Game Started! Good Luck ");
@@ -17,6 +19,26 @@ namespace SnackLadder
             Random Random = new Random();
             RanDomDice = Random.Next(1, 7);
             Console.WriteLine("player Role Dice number is:" + " " + RanDomDice);
+            ComputerChoice = Random.Next(0, 3);
+            Console.WriteLine("Dice value:" + ComputerChoice);
+            switch (ComputerChoice)
+            {
+                case 0:
+                    Console.WriteLine("No Play");
+                    break;
+                case 1:
+                    StartPoint += RanDomDice;
+                    Console.WriteLine("Got Ladder:" + StartPoint);
+                    break;
+                case 2:
+                    StartPoint -= RanDomDice;
+                    Console.WriteLine("Snake Attack:" + StartPoint);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option");
+                    break;
+            }
         }
     }
+    
 }

c800866a1e6d5ad8f08130db8416d8254a221dfa
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-05 21:34:10 +0530

    [Roshan]Added:Ensure The Player Get Exact Wining Possition

--- SnackLadder/SnakeAndLadder.cs
+++ SnackLadder/SnakeAndLadder.cs
@@ -28,20 +28,27 @@ namespace SnackLadder
                     break;
                 case 1:
                     StartPoint += RanDomDice;
+                    if (StartPoint > 100)
+                    {
+
+                        StartPoint -= StartPoint;
+                    }
+                    
                     Console.WriteLine("Got Ladder:" + StartPoint);
                     break;
                 case 2:
                     StartPoint -= RanDomDice;
+                    if (StartPoint < 0)
+                    {
+                        StartPoint = 0;
+                    }
                     Console.WriteLine("Snake Attack:" + StartPoint);
                     break;
                 default:
                     Console.WriteLine("Invalid Option");
                     break;
             }
-            if (StartPoint < 0)
-            {
-                StartPoint = 0;
-            }
+            
         }
     }
     

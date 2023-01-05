4a08fd8550f1669be103d25c1ef7b11f0fa7ab08
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-05 21:44:39 +0530

    [Roshan]Added:Report The NUmber Of Times Die Was Played To Win The Game

--- SnackLadder/SnakeAndLadder.cs
+++ SnackLadder/SnakeAndLadder.cs
@@ -8,48 +8,53 @@ namespace SnackLadder
 {
     public class SnakeAndLadder
     {
-        public int StartPoint = 0;
-        int RanDomDice;
-        int ComputerChoice;
-        
         public void StartPlaying()
         {
+            int StartPoint = 0;
+            int RanDomDice;
+            int ComputerChoice;
+            int count = 0;
             Console.WriteLine("Game Started! Good Luck ");
-            Console.WriteLine("You are at " + StartPoint);
-            Random Random = new Random();
-            RanDomDice = Random.Next(1, 7);
-            Console.WriteLine("player Role Dice number is:" + " " + RanDomDice);
-            ComputerChoice = Random.Next(0, 3);
-            Console.WriteLine("Dice value:" + ComputerChoice);
-            switch (ComputerChoice)
-            {
-                case 0:
-                    Console.WriteLine("No Play");
-                    break;
-                case 1:
-                    StartPoint += RanDomDice;
-                    if (StartPoint > 100)
-                    {
 
-                        StartPoint -= StartPoint;
-                    }
-                    
-                    Console.WriteLine("Got Ladder:" + StartPoint);
-                    break;
-                case 2:
-                    StartPoint -= RanDomDice;
-                    if (StartPoint < 0)
-                    {
-                        StartPoint = 0;
-                    }
-                    Console.WriteLine("Snake Attack:" + StartPoint);
-                    break;
-                default:
-                    Console.WriteLine("Invalid Option");
-                    break;
+            while (StartPoint != 100)
+            {
+                Random Random = new Random();
+                RanDomDice = Random.Next(1, 7);
+                Console.WriteLine("player Role Dice number is:" + " " + RanDomDice);
+                ComputerChoice = Random.Next(0, 3);
+                Console.WriteLine("Dice value:" + ComputerChoice);
+                switch (ComputerChoice)
+                {
+                    case 0:
+                        Console.WriteLine("No Play");
+                        count++;
+                        break;
+                    case 1:
+                        StartPoint += RanDomDice;
+                        count++;
+                        if (StartPoint > 100)
+                        {
+
+                            StartPoint -= StartPoint;
+                        }
+                        Console.WriteLine("Got Ladder and Position of the Player: " + StartPoint);
+                        break;
+                    case 2:
+                        StartPoint -= RanDomDice;
+                        count++;
+                        if (StartPoint < 0)
+                        {
+                            StartPoint = 0;
+                        }
+                        Console.WriteLine("Snake Attack and Position of the Player: " + StartPoint);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+                Console.WriteLine("Dice Role to Win the Game is " + count);
             }
-            
         }
     }
-    
+
 }
